0485a22ebe9ceec2e9d89f7866e87a22622e81fb
Aqib Rana <aaqibranaofficial@gmail.com>
2019-10-01 03:04:19 +0500

    Create Square.c
    Program to print square

---
+++ Square.c
@@ -0,0 +1,23 @@
+// Program to print square
+
+#include<stdio.h>
+int main()
+{
+    // Declaring Variables
+    int i, j;
+
+    // For Loop for rows
+    for(i=1;i<=5;i++)
+    {
+
+        // For loop for columns
+        for(j=1;j<=5;j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+
+    }
+
+}
