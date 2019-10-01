3467f7652be6b16e74dd432b845c4be100946640
Aqib Rana <aaqibranaofficial@gmail.com>
2019-10-02 02:13:36 +0500

    Create Printing_Numbers.c
    Program to print 1234 in three different methods

---
+++ Printing_Numbers.c
@@ -0,0 +1,18 @@
+// Program to print 1234 in three different methods
+
+#include<stdio.h>
+int main()
+{
+    // Using one printf statement with no conversion specifiers.
+    printf("1234\n");
+    
+    // Using one printf statement with four conversion specifiers.
+    printf("%d%d%d%d\n",1,2,3,4);
+    
+    // Using four printf statements
+    printf("1");
+    printf("2");
+    printf("3");
+    printf("4");
+
+}
