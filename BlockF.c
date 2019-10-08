a30b43b6760deda0dd8f92acfc2255abde9949ab
Aqib Rana <aaqibranaofficial@gmail.com>
2019-10-09 00:48:37 +0500

    Create BlockF.c
    Write a C program to print a block F using hash (#), where the F has a height of six characters and width of five and four characters.

---
+++ BlockF.c
@@ -0,0 +1,19 @@
+//Write a C program to print a block F using hash (#), where the F has a height of six characters and width of five and four characters.
+/*
+Expected Output:
+
+######
+#
+#
+#####
+#
+#
+#
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("#####\n#\n#\n####\n#\n#\n#\n");
+    return 0;
+}
