4239b904a03685571eb049110f3202bc306d30ce
Aqib Rana <aaqibranaofficial@gmail.com>
2019-10-09 00:47:10 +0500

    Create InfoDisplay.c
    Write a C program to print your name, date of birth. and mobile number.

---
+++ InfoDisplay.c
@@ -0,0 +1,16 @@
+// Write a C program to print your name, date of birth. and mobile number.
+/*
+Expected Output:
+
+Name   : Muhammad Aqib
+DOB    : June 02, 2001
+Mobile : 0305-5475970
+*/
+#include<stdio.h>
+int main()
+{
+    printf("Name \t :  Muhammad Aqib\n");
+    printf("DOB  \t :  June 02, 2001\n");
+    printf("Mobile   :  0305-5475970\n");
+    return 0;
+}
